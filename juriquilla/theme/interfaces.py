48baeaae7056f3a4b454feeb6716424d9fdae218
gil-cano <gil.cano@gmail.com>
2016-09-07 12:44:37 -0500

    initial theme

---
+++ juriquilla/theme/interfaces.py
@@ -0,0 +1,5 @@
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class IJuriquillaTheme(IDefaultBrowserLayer):
+    """Marker interface that defines a Zope 3 browser layer."""
--- setup.py
+++ setup.py
@@ -1,35 +1,43 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""Installer for the juriquilla.theme package."""
+
+from setuptools import find_packages
+from setuptools import setup
 
 version = '1.0dev0'
 
-setup(name='jurilla.theme',
-      version=version,
-      description="Diazo Theme Package for campus Juriquilla",
-      long_description="",
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='',
-      author='Informática Académica',
-      author_email='informatica.academica@matem.unam.mx',
-      url='https://github.com/imatem/juriquilla.theme',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['juriquilla'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.theming',
-          'z3c.jbot',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='jurilla.theme',
+    version=version,
+    description="Theme Package for campus Juriquilla",
+    long_description="",
+    # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='',
+    author='Informática Académica',
+    author_email='informatica.academica@matem.unam.mx',
+    url='https://github.com/imatem/juriquilla.theme',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['juriquilla'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'setuptools',
+      'plone.app.theming',
+      'z3c.jbot',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
